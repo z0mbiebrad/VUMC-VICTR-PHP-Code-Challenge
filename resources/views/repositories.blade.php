f4c88e6df88182a51f0f86698a7d1745a754ea06
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-07-13 14:12:10 -0400

    Updated Bootstrap to latest version

--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP All-Stars</title>
     <script></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
 </head>
 
@@ -19,7 +19,7 @@
     </div>
 
     <table
-        class="mt-2 table-bordered th, table-bordered td { border: 2px solid black } mx-auto table-sm w-25 text-left">
+        class="mt-2 table-bordered th, table-bordered td { border: 1px solid black } mx-auto table-sm w-25 text-left">
         @foreach ($repositories as $repository)
             <div class="accordion" id="accordion">
                 <tr class="accordion-item">
@@ -67,6 +67,11 @@
         {!! $repositories->links('pagination::bootstrap-5') !!}
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+    integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+</script>
 
 </html>
